b2cac338ab566844b9298a6d5c1920eacdac85ba
Diego Daniel Caceres <diegodanielcaceres10@gmail.com>
2026-09-13 12:50:11 +0200

    feat: add dotenv support for environment variables and update dependencies

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    publishableKey: dotenv.env['SUPABASE_PUBLISHABLE_KEY']!,
+  );
   runApp(const LumaApp());
 }
